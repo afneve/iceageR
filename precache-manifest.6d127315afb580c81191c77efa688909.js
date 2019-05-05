8c5d523be47be7d95ad8c60ea4c0ebf925c2dea8
Adam <afneve@gmail.com>
2019-05-05 18:28:18 -0500

    Updates

---
+++ precache-manifest.6d127315afb580c81191c77efa688909.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb92a726700e1ad96c05",
+    "url": "/iceageR/static/js/runtime~main.cc6ea412.js"
+  },
+  {
+    "revision": "71013f8f0adf852a2740",
+    "url": "/iceageR/static/js/main.73aa0ff6.chunk.js"
+  },
+  {
+    "revision": "3cbe45b9d985f04548a9",
+    "url": "/iceageR/static/js/2.552e3b0b.chunk.js"
+  },
+  {
+    "revision": "71013f8f0adf852a2740",
+    "url": "/iceageR/static/css/main.098ff4cf.chunk.css"
+  },
+  {
+    "revision": "563cbc82bcc6fe0bbe68150fc3acf483",
+    "url": "/iceageR/index.html"
+  }
+];

c33a049ea7021dcce5d3ea7b727fb3bb0b9b31f5
Adam <afneve@gmail.com>
2022-07-03 15:29:12 -0500

    Adding percentage

--- src/components/Containers/Home.jsx
+++ src/components/Containers/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
         <div className='Home'>
             <div>
                 <h2 className='user-miles-remaining'>{`${(totalMiles - totalMilesCompleted).toFixed(1)} miles to go`}</h2>
-                <div>{`${totalMilesCompleted} of ${totalMiles.toFixed(1)} miles completed`}</div>
+                <div>{`${totalMilesCompleted} of ${totalMiles.toFixed(1)} miles completed (${((totalMilesCompleted / totalMiles.toFixed(1)) * 100).toFixed(1)}%)`}</div>
                 <div>{`${(totalNumberOfSegments - totalNumberOfCompletedSegments)} segments remaining`}</div>
                 <div>{`${partialSegmentMiles.toFixed(1)} miles of partially completed segments`}</div>
             </div>

8aadc3a950e432d7b76831f8465ac63afb663829
Adam <afneve@gmail.com>
2024-09-30 21:45:54 -0500

    Small tweak to display of home page

--- src/components/Containers/Home.jsx
+++ src/components/Containers/Home.jsx
@@ -1,7 +1,9 @@
-import { iceAgeData } from '../../data/ice_age_data';
+import { iceAgeData } from "../../data/ice_age_data";
 
-import { selectCompletedSegments, selectPartialSegments } from '../../utils/getIceAgeData';
-
+import {
+    selectCompletedSegments,
+    selectPartialSegments,
+} from "../../utils/getIceAgeData";
 
 const Home = () => {
     const completedSegments = selectCompletedSegments();
@@ -11,31 +13,55 @@ const Home = () => {
     const totalNumberOfCompletedSegments = completedSegments.length;
 
     const partialSegmentMiles = partialSegments.reduce(
-        (previousValue, currentValue) => Number(previousValue) + Number(currentValue.iceagetraildistance),
+        (previousValue, currentValue) =>
+            Number(previousValue) + Number(currentValue.iceagetraildistance),
         0
-    )
+    );
 
     const totalMilesCompleted = completedSegments.reduce(
-        (previousValue, currentValue) => Number(previousValue) + Number(currentValue.iceagetraildistance),
+        (previousValue, currentValue) =>
+            Number(previousValue) + Number(currentValue.iceagetraildistance),
         0
-    )
+    );
 
     const totalMiles = iceAgeData.reduce(
-        (previousValue, currentValue) => Number(previousValue) + Number(currentValue.iceagetraildistance),
+        (previousValue, currentValue) =>
+            Number(previousValue) + Number(currentValue.iceagetraildistance),
         0
-    )
+    );
 
     return (
-        <div className='Home'>
+        <div className="Home">
             <div>
-                <h2 className='user-miles-remaining'>{`${(totalMiles - totalMilesCompleted).toFixed(1)} miles to go`}</h2>
-                <div>{`${totalMilesCompleted.toFixed(1)} of ${totalMiles.toFixed(1)} miles completed (${((totalMilesCompleted / totalMiles.toFixed(1)) * 100).toFixed(1)}%)`}</div>
-                <div>{`${(totalNumberOfSegments - totalNumberOfCompletedSegments)} segments remaining`}</div>
-                <div>{`${partialSegmentMiles.toFixed(1)} miles of partially completed segments`}</div>
+                <h2 className="user-miles-remaining">
+                    <strong>{`${(totalMiles - totalMilesCompleted).toFixed(
+                        1
+                    )} `}</strong>{" "}
+                    miles to go
+                </h2>
+                <p>
+                    <strong>{`${totalMilesCompleted.toFixed(1)}`}</strong> of{" "}
+                    <strong>{`${totalMiles.toFixed(1)}`}</strong> miles
+                    completed (
+                    <strong>{`${(
+                        (totalMilesCompleted / totalMiles.toFixed(1)) *
+                        100
+                    ).toFixed(1)}%`}</strong>
+                    )
+                </p>
+                <p>
+                    <strong>{`${
+                        totalNumberOfSegments - totalNumberOfCompletedSegments
+                    }`}</strong>{" "}
+                    segments remaining
+                </p>
+                <p>
+                    <strong>{`${partialSegmentMiles.toFixed(1)}`}</strong> miles
+                    of partially completed segments
+                </p>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
 export default Home;
